b691c5c24c4121836df1276df5b8162cdec49f12
houyen <34957287+houyen@users.noreply.github.com>
2025-12-18 14:02:59 +0000

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Basic JavaScript for the portfolio
+console.log("Portfolio loaded");
+
+// Example function
+function greet() {
+    alert("Welcome to my portfolio!");
+}
+
+// You can add more JavaScript here
